037f9c140398bb2deb97ba52766fde8e8103cd1f
Cleidson Oliveira <gk.cleidson@yahoo.com.br>
2020-07-17 14:05:34 -0300

    Google OAuth2 Implements

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const passport = require('passport'),
+  GoogleStrategy = require('passport-google-oauth20').Strategy,
+  keys = require('./config/keys');
+
 const app = express();
+
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientId,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback'
+}, (acessToken) => {
+  console.log(acessToken)
+}));
 
 app.get('/', (req, res) => {
   res.send(`<p>Hello World!<p/>`);
 });
 
-const PORT = process.env.PORT || 5008;
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+}));
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+console.log(`Server is on port ${PORT}`)
