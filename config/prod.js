207e28674cf34b146eb59dd42e87587af348813e
Cleidson Oliveira <gk.cleidson@yahoo.com.br>
2020-07-18 12:30:37 -0300

    Some Changes DB

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
   googleClientId: '911612768701-7lkdqgdj4v3r4jsksd2rdvgvv5ijte4m.apps.googleusercontent.com',
   googleClientSecret: 'AB04d53DcEj3CBgUOZrpSHdn',
-  mongoURI: 'mongodb+srv://utbEzoTeo:TBjiRkfRx6cfG3LC@emailly-prod.zpi4h.gcp.mongodb.net/emailly-production?retryWrites=true&w=majority',
+  mongoURI: 'mongodb+srv://utbEzoTeo:TBjiRkfRx6cfG3LC@emailly-prod.zpi4h.gcp.mongodb.net/emaillyproduction?retryWrites=true&w=majority',
   cookieKey: 'oePaenaemonTeoooZMAQoWgbvi'
 }
