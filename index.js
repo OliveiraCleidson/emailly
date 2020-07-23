c32698a2c73f6aa1d3c32c42045ea7d8521a1790
Cleidson Oliveira <gk.cleidson@yahoo.com.br>
2020-07-23 01:06:12 -0300

    Final Hotfix

--- index.js
+++ index.js
@@ -13,12 +13,10 @@ globalMiddlewares(app, express);
 // Routes
 if (process.env.NODE_ENV === 'production') {
   const path = require('path');
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client/build')));
   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
-
 routes(app);
-console.log(process.env.NODE_ENV);
 
 app.get('/', (req, res) => {
   res.send(`<p>Hello World!<p/>`);
